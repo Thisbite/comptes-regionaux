72c8cbb09e2b1a31f7aa87bd71675ef5a5c3a108
Toh bi <iriefrancktohbi@gmail.com>
2024-06-18 12:11:22 +0000

    first commit4

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ if not st.session_state['logged_in']:
 else:
     if st.session_state['role'] == 'admin':
         page = st.sidebar.selectbox("Choisir une page",
-                                    ["Saisi de données", "Importer le fichier Excel", "Statistiques","Modifier données","Enregistrement"])
+                                    ["Saisi de données", "Importer le fichier Excel", "Statistiques","Modifier données","Enregistrement",
+                                     "Annuaire statistique"])
     elif st.session_state['role'] == 'employe':
         page = st.sidebar.selectbox("Choisir une page", ["Performance des Ouvriers"])
 
@@ -162,18 +163,68 @@ elif page=="Modifier données":
         st.success(
             f"Les données pour la direction '{direction}', l'année '{annee}' et le code '{code}' ont été modifiées avec succès!")
 
-    # Affichage des données enregistrées
+elif page== "Annuaire statistique":
+    # Section pour télécharger un fichier Excel
+    st.subheader("Télécharger un fichier Excel")
+    uploaded_file = st.file_uploader("Télécharger un fichier Excel", type=["xlsx"],key="existence_partis")
 
-elif st.checkbox("Supprimer une données"):
-    st.subheader("Supprimer des enregistrements")
-    with st.form("delete_form"):
-        direction = st.text_input("Direction")
-        annee = st.text_input("Année")
-        code = st.text_input("Code")
-        submit_delete = st.form_submit_button("Supprimer")
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
 
-    # Suppression des données lorsque le bouton est cliqué
-    if submit_delete:
-        data.supprimer_agriculture_vivriere(direction, annee, code)
-        st.success(
-            f"Les données pour la direction '{direction}', l'année '{annee}' et le code '{code}' ont été supprimées avec succès!")
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "partis_politique", "Annee_2015", "Annee_2016", "Annee_2017",
+                            "Annee_2018", "Annee_2019"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_existence_partis(
+                        row['direction'], row['region'], row['partis_politique'],
+                        row['Annee_2015'], row['Annee_2016'], row['Annee_2017'],
+                        row['Annee_2018'], row['Annee_2019']
+                    )
+                st.success("Les données du fichier ont été enregistrées avec succès!")
+        else:
+            st.error(
+                "Les colonnes du fichier ne correspondent pas aux colonnes attendues. Veuillez vérifier votre fichier.")
+
+    st.subheader("Télécharger un fichier excel :Naissances enregistrées et parvenus au niveau central selon le type de centre de la Région du Poro par Département et par Sous-Préfecture en 2019")
+    uploaded_file = st.file_uploader("Télécharger un fichier Excel", type=["xlsx"],key="faits_naissace")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "departement", "sous_prefecture", "faits_civil",
+                            "type_de_centre_civil", "dans_les_delais_3_mois", "hors_delai_4_12_mois",
+                            "hors_delai_plus_de_12_mois", "total_faits_naissance"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_faits_civils(
+                        row['direction'], row['region'], row['departement'], row['sous_prefecture'],
+                        row['faits_civil'], row['type_de_centre_civil'], row['dans_les_delais_3_mois'],
+                        row['hors_delai_4_12_mois'], row['hors_delai_plus_de_12_mois'], row['total_faits_naissance']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    st.subheader("Tableau 1.2.1: Existence des partis  politiques par département sur les 5 dernières années 2015-2019")
+
+    st.write("1=présent ; 0=absent")
+    st.write(data.obtenir_existence_partis())
+
+    st.subheader("Tableau 2.1.13 : Naissances enregistrées et parvenus au niveau central selon le type de centre de la Région du Poro par Département et par Sous-Préfecture en 2019")
+    st.write(data.obtenir_faits_civils())
--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 import sqlite3
 import pandas as pd
 import hashlib
-conn = sqlite3.connect("comptes_regionaux.db")
-cursor = conn.cursor()
+
+DATABASE = "comptes_regionaux.db"
 
 def creer_table():
-    # Table agriculture vivrière
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute(
         '''
         CREATE TABLE IF NOT EXISTS agriculture_vivrieres(
@@ -32,36 +33,71 @@ def creer_table():
                         role TEXT
                       )''')
 
-
-
-
-
-
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS existence_partis(
+        id INTEGER PRIMARY KEY ,
+        direction TEXT,
+        region TEXT,
+        partis_politique TEXT,
+        Annee_2015 TEXT,
+        Annee_2016 TEXT,
+        Annee_2017 TEXT,
+        Annee_2018 TEXT,
+        Annee_2019 TEXT
+        )
+        '''
+    )
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS faits_civils(
+        id INTEGER PRIMARY KEY ,
+        direction TEXT,
+        region TEXT,
+        departement TEXT,
+        sous_prefecture TEXT,
+        faits_civil TEXT,
+        type_de_centre_civil TEXT,
+        dans_les_delais_3_mois INTEGER,
+        hors_delai_4_12_mois INTEGER,
+        hors_delai_plus_de_12_mois INTEGER,
+        total_faits_naissance INTEGER
+        )
+        '''
+    )
 
     conn.commit()
+    conn.close()
 
 def enregistrer_agriculture_vivirier(direction, region, annee, codes, libelle_produit, quantite, valeur,
                                      prix_unitaire, superficie, effectif, masse_salariale):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''
         INSERT INTO agriculture_vivrieres(direction, region, annee, codes, libelle_produit, quantite, valeur, prix_unitaire, superficie, effectif, masse_salariale)
         VALUES(?,?,?,?,?,?,?,?,?,?,?)''', (
         direction, region, annee, codes, libelle_produit, quantite, valeur, prix_unitaire, superficie, effectif, masse_salariale)
     )
     conn.commit()
+    conn.close()
 
 def obtenir_agriculture_vivriere():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('SELECT * FROM agriculture_vivrieres')
     data = cursor.fetchall()
     df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Codes", "Libellé du produit",
                                      "Quantité", "Valeur", "Prix unitaire", "Superficie",
                                      "Effectifs employés", "Masse salariale"])
+    conn.close()
     return df
 
 # Créer la table
 creer_table()
 
-
 def obtenir_statistiques_riz():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''
         SELECT region, annee, SUM(quantite) as quantite
         FROM agriculture_vivrieres
@@ -71,71 +107,130 @@ def obtenir_statistiques_riz():
     ''')
     data = cursor.fetchall()
     df = pd.DataFrame(data, columns=["region", "Année", "quantite"])
+    conn.close()
     return df
 
 def obtenir_statistiques_par_code(code):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''
         SELECT region, annee, SUM(quantite) as quantite
         FROM agriculture_vivrieres
         WHERE codes = ?
         GROUP BY region, annee
-        ORDER BY annee,quantite DESC
+        ORDER BY annee, quantite DESC
     ''', (code,))
     dat = cursor.fetchall()
     df = pd.DataFrame(dat, columns=["region", "Année", "quantite"])
+    conn.close()
     return df
 
-
-
 def modifier_agriculture_vivriere(direction, annee, code, new_values):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''
         UPDATE agriculture_vivrieres
         SET direction = ?, region = ?, annee = ?, codes = ?, libelle_produit = ?, quantite = ?, valeur = ?, prix_unitaire = ?, superficie = ?, effectif = ?, masse_salariale = ?
         WHERE direction = ? AND annee = ? AND codes = ?
     ''', (*new_values, direction, annee, code))
     conn.commit()
-
-
+    conn.close()
 
 def supprimer_agriculture_vivriere(direction, annee, code):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''
         DELETE FROM agriculture_vivrieres
         WHERE direction = ? AND annee = ? AND codes = ?
     ''', (direction, annee, code))
     conn.commit()
-
-
+    conn.close()
 
 def enregistrer_utilisateur(username, password, role):
     password_hashed = hashlib.sha256(password.encode()).hexdigest()
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''INSERT INTO utilisateurs (username, password, role)
                       VALUES (?, ?, ?)''', (username, password_hashed, role))
     conn.commit()
+    conn.close()
 
-#enregistrer_utilisateur("admin2","admin2", "admin")
 def verifier_utilisateur(username, password):
     password_encoded = password.encode('utf-8')
     password_hashed = hashlib.sha256(password_encoded).hexdigest()
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('''SELECT * FROM utilisateurs WHERE username=? AND password=?''', (username, password_hashed))
     user = cursor.fetchone()
+    conn.close()
     if user:
         return {'id': user[0], 'username': user[1], 'role': user[3]}
     return None
 
 def obtenir_region():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('SELECT DISTINCT region FROM agriculture_vivrieres ')
-    dat=cursor.fetchall()
-    region=[row[0] for row in dat]
+    dat = cursor.fetchall()
+    conn.close()
+    region = [row[0] for row in dat]
     return region
+
 def obtenir_direction():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('SELECT DISTINCT direction FROM agriculture_vivrieres ')
-    dat=cursor.fetchall()
-    direction=[row[0] for row in dat]
+    dat = cursor.fetchall()
+    conn.close()
+    direction = [row[0] for row in dat]
     return direction
 
-
 def obtenir_code():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
     cursor.execute('SELECT DISTINCT codes FROM agriculture_vivrieres ')
-    dat=cursor.fetchall()
-    codes=[row[0] for row in dat]
+    dat = cursor.fetchall()
+    conn.close()
+    codes = [row[0] for row in dat]
     return codes
+
+def enregistrer_existence_partis(direction, region, partis_politique, annee_2015, annee_2016, annee_2017, annee_2018, annee_2019):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO existence_partis(direction, region, partis_politique, Annee_2015, Annee_2016, Annee_2017, Annee_2018, Annee_2019)
+        VALUES(?,?,?,?,?,?,?,?)
+    ''', (direction, region, partis_politique, annee_2015, annee_2016, annee_2017, annee_2018, annee_2019))
+    conn.commit()
+    conn.close()
+
+def obtenir_existence_partis():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM existence_partis')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Partis Politique", "Année 2015", "Année 2016", "Année 2017", "Année 2018", "Année 2019"])
+    conn.close()
+    return df
+
+
+
+def enregistrer_faits_civils(direction, region, departement, sous_prefecture, faits_civil, type_de_centre_civil, dans_les_delais_3_mois, hors_delai_4_12_mois, hors_delai_plus_de_12_mois, total_faits_naissance):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO faits_civils(direction, region, departement, sous_prefecture, faits_civil, type_de_centre_civil, dans_les_delais_3_mois, hors_delai_4_12_mois, hors_delai_plus_de_12_mois, total_faits_naissance)
+        VALUES(?,?,?,?,?,?,?,?,?,?)
+    ''', (direction, region, departement, sous_prefecture, faits_civil, type_de_centre_civil, dans_les_delais_3_mois, hors_delai_4_12_mois, hors_delai_plus_de_12_mois, total_faits_naissance))
+    conn.commit()
+    conn.close()
+
+
+def obtenir_faits_civils():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM faits_civils')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Département", "Sous-préfecture", "Faits Civil", "Type de Centre Civil", "Dans les délais (3 mois)", "Hors délai (4-12 mois)", "Hors délai (plus de 12 mois)", "Total Faits Naissance"])
+    conn.close()
+    return df
