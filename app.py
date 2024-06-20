77de22462f76597d7db36ca3f6515eaeea1e3bba
Toh bi <iriefrancktohbi@gmail.com>
2024-06-20 17:25:05 +0000

    Mise à jour de l'annauire

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import data
 import pandas as pd
-from io import BytesIO
+
 
 # Titre de l'application
 st.set_page_config(page_title="comptes regionaux", page_icon="👗", layout="wide")
@@ -16,7 +16,7 @@ if not st.session_state['logged_in']:
 else:
     if st.session_state['role'] == 'admin':
         page = st.sidebar.selectbox("Choisir une page",
-                                    ["Nouveau utilisateur","Annuaire statistique"])
+                                    ["Annuaire statistique","Nouveau utilisateur"])
 
 
 if page == "Connexion":
@@ -142,6 +142,259 @@ elif page== "Annuaire statistique":
         else:
             st.error("Le fichier Excel ne contient pas les colonnes requises.")
 
+
+    st.write("Tableau 1.2.3: Effectif des maires élus  par département et par sexe en  exercices municipaux")
+    uploaded_file = st.file_uploader("Importer les données ", type=["xlsx"],
+                                     key="tab_effec_maire_depart")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "total_sexe", "femmes"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_effec_maire_depart(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['total_sexe'],
+                        row['femmes']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+
+    st.write("Tableau 1.2.4: Répartition régionale des postes de responsabilités occupés par les femmes dans les mairies (maire, 1er adjoint au maire, 2ème adjoint au maire) aux exercices municipaux ")
+    uploaded_file = st.file_uploader("Importer les données", type=["xlsx"],
+                                     key="tab_repa_post_mai_fem_dep")
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "fonction", "total_sexe", "femmes"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enreg_tab_repa_post_mai_fem_dep(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['fonction'],
+                        row['total_sexe'], row['femmes']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées"):
+        df_data = data.obtenir_tab_repa_post_mai_fem_dep()
+        st.dataframe(df_data)
+
+
+
+    st.write("Tableau 1.2.5: Répartition des maires par département, par sexe et par partie politique (on pourrait aussi voir l'évolution)")
+    uploaded_file = st.file_uploader("Importer les données ", type=["xlsx"],
+                                     key="tab_repa_mair_dep_pol")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "partis_politique", "hommes", "femmes",
+                            "total_sexe"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_repa_mair_dep_pol(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['partis_politique'],
+                        row['hommes'], row['femmes'], row['total_sexe']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées",key="tab125"):
+        df_data = data.obtenir_tab_repa_mair_dep_pol()
+        st.dataframe(df_data)
+
+
+    st.write("Tableau 1.2.6: Répartition des adjoints au maire par département, par sexe et par partie politique ")
+    uploaded_file = st.file_uploader("Importer les données ", type=["xlsx"],
+                                     key="tab_repa_adj_mai_dep_p")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "partis_politique", "hommes", "femmes",
+                            "total_sexe"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_repa_adj_mai_dep_p(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['partis_politique'],
+                        row['hommes'], row['femmes'], row['total_sexe']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées",key="tab126"):
+        df_data = data.obtenir_tab_repa_adj_mai_dep_p()
+        st.dataframe(df_data)
+
+
+    st.write("Tableau 1.2.7 : Effectif des maires reconduits et nouvellement élus par département")
+    uploaded_file = st.file_uploader("Importer les données ", type=["xlsx"],
+                                     key="tab_mair_recon_nvl_dep")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "maire_nouveau", "maire_reconduit"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_mair_recon_nvl_dep(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['maire_nouveau'],
+                        row['maire_reconduit']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées",key="tab127"):
+        df_data = data.obtenir_tab_mair_recon_nvl_dep()
+        st.dataframe(df_data)
+
+
+
+
+    st.write("Tableau 1.2.8: Répartition des députés par sexe et par partie politique")
+    uploaded_file = st.file_uploader("Importer des données (format Excel)", type=["xlsx"])
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "partis_politique", "hommes", "femmes",
+                            "total_sexe"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_repa_deput_pol_sex_dep(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['partis_politique'],
+                        row['hommes'], row['femmes'], row['total_sexe']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées",key="tab128"):
+        df_data = data.obtenir_tab_repa_deput_pol_sex_dep()
+        st.dataframe(df_data)
+
+
+    st.write("Tableau 1.2.9: Liste des sous-préfectures par département")
+    uploaded_file = st.file_uploader("Importer des données (format Excel)", type=["xlsx"],key="tab129")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "list_sous_pref_urbain",
+                            "list_sous_pref_rural", "nombre_sous_pref"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_list_sous_pref_dep(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['list_sous_pref_urbain'],
+                        row['list_sous_pref_rural'], row['nombre_sous_pref']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées",key="tabl129"):
+        df_data =data.obtenir_tab_list_sous_pref_dep()
+        st.dataframe(df_data)
+
+    st.write("Tableau 1.2.10: Superficie et nombre de village par sous-préfecture")
+    uploaded_file = st.file_uploader("Importer des données (format Excel)", type=["xlsx"], key="tab1210")
+
+    if uploaded_file is not None:
+        # Lire le fichier Excel
+        df = pd.read_excel(uploaded_file)
+
+        # Afficher les données du fichier
+        st.dataframe(df)
+
+        # Vérifier les colonnes du fichier
+        expected_columns = ["direction", "region", "annee", "departement", "sous_prefecture",
+                            "superficie", "nombre_village"]
+
+        if all(column in df.columns for column in expected_columns):
+            # Bouton pour enregistrer les données dans la base de données
+            if st.button("Enregistrer les données dans la base de données"):
+                for _, row in df.iterrows():
+                    data.enregistrer_tab_superf_nb_vil_dep(
+                        row['direction'], row['region'], row['annee'], row['departement'], row['sous_prefecture'],
+                        row['superficie'], row['nombre_village']
+                    )
+                st.success("Données enregistrées avec succès!")
+        else:
+            st.error("Le fichier Excel ne contient pas les colonnes requises.")
+
+    # Option pour afficher les données enregistrées
+    if st.checkbox("Afficher les données enregistrées", key="tabl1210"):
+        df_data = data.obtenir_tab_superf_nb_vil_dep()
+        st.dataframe(df_data)
 
     #st.subheader("Tableau 1.2.1: Existence des partis  politiques par département sur les 5 dernières années 2015-2019")
 
--- data.py
+++ data.py
@@ -26,11 +26,224 @@ def verifier_utilisateur(username, password):
         return {'id': user[0], 'username': user[1], 'role': user[3]}
     return None
 
+def reset_table():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_repa_mair_dep_pol(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        partis_politique TEXT,
+        hommes TEXT,
+        femmes TEXT,
+        total_sexe TEXT
+        )
+        '''
+    )
+    conn.commit()
+    conn.close()
+
+
+
 
 
 def creer_table():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
+
+    # Tableau 1.2.10: Superficie et nombre de village par sous-préfecture
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_superf_nb_vil_dep(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        sous_prefecture TEXT,
+        superficie TEXT,
+        nombre_village INTEGER
+
+
+
+        )
+
+        '''
+    )
+
+
+
+
+#Tableau 1.2.9: Liste des sous-préfectures par département
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_list_sous_pref_dep(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        list_sous_pref_urbain TEXT,
+        list_sous_pref_rural TEXT,
+       nombre_sous_pref TEXT
+        
+
+
+        )
+
+        '''
+    )
+    #Tableau 1.2.8: Répartition des députés par sexe et par partie politique
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_repa_deput_pol_sex_dep(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        partis_politique TEXT,
+        hommes TEXT,
+        femmes TEXT,
+        total_sexe TEXT
+
+
+        )
+
+        '''
+    )
+
+
+
+
+
+#Tableau 1.2.7 : Effectif des maires reconduits et nouvellement élus par département
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_mair_recon_nvl_dep(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        maire_nouveau INTEGER,
+        maire_reconduit INTEGER
+        
+
+
+        )
+
+        '''
+    )
+
+
+
+
+
+
+#Tableau 1.2.6: Répartition des adjoints au maire par département, par sexe et par partie politique
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_repa_adj_mai_dep_p(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        partis_politique TEXT,
+        hommes TEXT,
+        femmes TEXT,
+        total_sexe TEXT
+
+
+        )
+
+        '''
+    )
+
+
+
+
+
+
+
+#Tableau 1.2.5: Répartition des maires par département, par sexe et par partie politique (on pourrait aussi voir l'évolution)
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_repa_mair_dep_pol(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        partis_politique TEXT,
+        hommes TEXT,
+        femmes TEXT,
+        total_sexe TEXT
+        
+
+        )
+
+        '''
+    )
+
+
+
+
+
+
+
+
+#Tableau 1.2.4: Répartition régionale des postes de responsabilités occupés par les femmes dans les mairies
+# (maire, 1er adjoint au maire, 2ème adjoint au maire) sur les 5 derniers exercices municipaux
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_repa_pos_mai_fem_dep(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+        fonction TEXT,
+        total_sexe TEXT,
+        femmes TEXT
+
+        )
+
+        '''
+    )
+
+
+#Tableau 1.2.3: Effectif des maires élus  par département et par sexe sur les 5 derniers exercices municipaux
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tab_effec_maire_depart(
+        id INTEGER PRIMARY KEY,
+        direction TEXT,
+        region TEXT,
+        annee TEXT,
+        departement TEXT,
+         total_sexe TEXT,
+        femmes TEXT
+
+        )
+
+        '''
+    )
+
+
+
+
 
 # Tableau 1.2.2 nombre de responsables politique departementaux
     cursor.execute(
@@ -50,11 +263,6 @@ def creer_table():
         
         '''
     )
-
-
-
-
-
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS utilisateurs (
                         id INTEGER PRIMARY KEY,
@@ -106,6 +314,206 @@ def creer_table():
 # Créer la table
 creer_table()
 
+# Fonction pour insérer des données dans la table
+
+def enregistrer_tab_superf_nb_vil_dep(direction, region, annee, departement, sous_prefecture, superficie, nombre_village):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_superf_nb_vil_dep (direction, region, annee, departement,sous_prefecture,superficie,nombre_village)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement,sous_prefecture, superficie,nombre_village)
+    )
+    conn.commit()
+    conn.close()
+
+# Fonction pour obtenir les données de la table
+def obtenir_tab_superf_nb_vil_dep():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_superf_nb_vil_dep')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Sous-Préfecture","Superficie(km2)", "Nombre de village"])
+    conn.close()
+    return df
+
+
+
+
+
+
+
+
+
+
+
+def enregistrer_tab_list_sous_pref_dep(direction, region, annee, departement, list_sous_pref_urbain, list_sous_pref_rural, nombre_sous_pref):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_list_sous_pref_dep (direction, region, annee, departement, list_sous_pref_urbain, list_sous_pref_rural, nombre_sous_pref)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, list_sous_pref_urbain, list_sous_pref_rural, nombre_sous_pref)
+    )
+    conn.commit()
+    conn.close()
+
+# Fonction pour obtenir les données de la table
+def obtenir_tab_list_sous_pref_dep():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_list_sous_pref_dep')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Liste Sous-Préfecture Urbain", "Liste Sous-Préfecture Rural", "Nombre Sous-Préf."])
+    conn.close()
+    return df
+
+
+
+def enregistrer_tab_repa_deput_pol_sex_dep(direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_repa_deput_pol_sex_dep (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+    )
+    conn.commit()
+    conn.close()
+
+# Fonction pour obtenir les données de la table
+def obtenir_tab_repa_deput_pol_sex_dep():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_repa_deput_pol_sex_dep')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Partis Politique", "Hommes", "Femmes", "Total Sexe"])
+    conn.close()
+    return df
+
+
+def enregistrer_tab_mair_recon_nvl_dep(direction, region, annee, departement, maire_nouveau, maire_reconduit):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_mair_recon_nvl_dep (direction, region, annee, departement, maire_nouveau, maire_reconduit)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, maire_nouveau, maire_reconduit)
+    )
+    conn.commit()
+    conn.close()
+
+def obtenir_tab_mair_recon_nvl_dep():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_mair_recon_nvl_dep')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Maire Nouveau", "Maire Reconduit"])
+    conn.close()
+    return df
+
+def enregistrer_tab_repa_adj_mai_dep_p(direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_repa_adj_mai_dep_p (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+    )
+    conn.commit()
+    conn.close()
+
+def obtenir_tab_repa_adj_mai_dep_p():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_repa_adj_mai_dep_p')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Partis Politique", "Hommes", "Femmes", "Total Sexe"])
+    conn.close()
+    return df
+
+def enregistrer_tab_repa_mair_dep_pol(direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_repa_mair_dep_pol (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, partis_politique, hommes, femmes, total_sexe)
+    )
+    conn.commit()
+    conn.close()
+
+
+def obtenir_tab_repa_mair_dep_pol():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_repa_mair_dep_pol')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Partis Politique", "Hommes", "Femmes", "Total Sexe"])
+    conn.close()
+    return df
+
+
+
+def enreg_tab_repa_post_mai_fem_dep(direction, region, annee, departement, fonction, total_sexe, femmes):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_repa_pos_mai_fem_dep (direction, region, annee, departement, fonction, total_sexe, femmes)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, fonction, total_sexe, femmes)
+    )
+    conn.commit()
+    conn.close()
+
+
+
+
+
+def obtenir_tab_repa_post_mai_fem_dep():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_repa_pos_mai_fem_dep')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Fonction", "Total Sexe", "Femmes"])
+    conn.close()
+    return df
+
+def enregistrer_tab_effec_maire_depart(direction, region, annee, departement, total_sexe, femmes):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO tab_effec_maire_depart (direction, region, annee, departement, total_sexe, femmes)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''',
+        (direction, region, annee, departement, total_sexe, femmes)
+    )
+    conn.commit()
+    conn.close()
+
+def obtenir_tab_effec_maire_depart():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tab_effec_maire_depart')
+    data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=["ID", "Direction", "Région", "Année", "Département", "Total Sexe", "Femmes"])
+    conn.close()
+    return df
 
 def enregistrer_tab_respo_pltq_dep(direction,region,annee,partis_politique,departement,hommes,femmes,total_sexe):
     conn = sqlite3.connect(DATABASE)
